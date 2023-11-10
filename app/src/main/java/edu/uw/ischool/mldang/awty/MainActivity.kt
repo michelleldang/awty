2bd92369cc8496e4b10b4c1e3217a7f9209b67e4
mldang <mldang@uw.edu>
2023-11-09 23:48:31 -0800

    push

---
+++ app/src/main/java/edu/uw/ischool/mldang/awty/MainActivity.kt
@@ -0,0 +1,116 @@
+package edu.uw.ischool.mldang.awty
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+const val ALARM_ACTION = "edu.uw.ischool.mldang.ALARM"
+
+class MainActivity : AppCompatActivity() {
+    lateinit var button : Button
+    lateinit var messageInput: EditText
+    lateinit var phoneInput: EditText
+    lateinit var minutesInput: EditText
+    var receiver : BroadcastReceiver? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.start_button)
+        messageInput = findViewById(R.id.message_input)
+        phoneInput = findViewById(R.id.phone_input)
+        minutesInput = findViewById(R.id.minutes_input)
+        button.setOnClickListener {
+            if (messageInput.text.toString() != "" || phoneInput.text.toString()!= "" || minutesInput.text.toString()!= "") {
+                validate(
+                    messageInput.text.toString(),
+                    phoneInput.text.toString(),
+                    minutesInput.text.toString()
+                )
+            }
+        }
+
+
+    }
+
+//    override fun onPause() {
+//        super.onPause()
+//
+//        unregisterReceiver(receiver)
+//        receiver = null
+//    }
+
+    private fun validate (message: String, phone: String, minutes: String) {
+        val  button: Button = findViewById(R.id.start_button)
+        errorMessages(message, phone, minutes)
+        if (message.trim() != "" && Regex("^(\\+\\d{1,2}\\s?)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}\$").matches(phone.trim()) && Integer.parseInt(minutes) > 0) {
+            if (button.text == "Start") {
+                startMessages(message, phone, Integer.parseInt(minutes))
+                button.text = "Stop"
+            } else {
+                button.text = "Start"
+                stopMessages()
+            }
+        } else if ( button.text == "Stop") {
+            button.text = "Start"
+            stopMessages()
+        }
+    }
+    private fun errorMessages(message: String, phone: String, minutes: String) {
+        if(message.trim() == "" ) {
+            messageInput.error = "Message field cannot be empty"
+        } else {
+            messageInput.error = null
+        }
+        if(!Regex("^(\\+\\d{1,2}\\s?)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}\$").matches(phone.trim()) || phone.trim() == "") {
+            phoneInput.error = "Phone number not in correct format"
+        } else {
+            phoneInput.error = null
+        }
+        if( Integer.parseInt(minutes) < 0 || minutes.trim() == "") {
+            minutesInput.error = "Minutes interval cannot be 0 or below"
+        } else {
+            minutesInput.error = null
+        }
+    }
+    private fun startMessages (message: String, phone: String, minutes: Int) {
+        val activityThis = this
+        if (receiver == null) {
+            receiver = object : BroadcastReceiver() {
+                override fun onReceive(context: Context?, intent: Intent?) {
+                    Log.d("MainActivity", "toast activity ")
+                    Toast.makeText(activityThis, "${phone}:${message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+            val filter = IntentFilter(ALARM_ACTION)
+            registerReceiver(receiver, filter)
+        }
+        val intent = Intent(ALARM_ACTION)
+        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val alarmManager : AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.setRepeating(
+            AlarmManager.RTC_WAKEUP,
+            System.currentTimeMillis(),
+            minutes.toLong(), //  1000 * 60 * minutes
+            pendingIntent)
+    }
+    private fun stopMessages () {
+
+    }
+}
+
+
+
+
+
